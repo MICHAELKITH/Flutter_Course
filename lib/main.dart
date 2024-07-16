46a887331d0783c9d290705dcf84dc5841025621
MICHAELKITH <michaelkithinji27@gmail.com>
2024-07-16 23:21:33 +0300

    Intial commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: '55 Blocks Inc'),
+      home: const MyHomePage(title: '55 Blocks Company'),
     );
   }
 }

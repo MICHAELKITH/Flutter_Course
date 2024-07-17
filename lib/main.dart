61e1d3d3384d1cb7ff86b68bc1e4eb392302116e
MICHAELKITH <michaelkithinji27@gmail.com>
2024-07-17 23:02:59 +0300

    Intial commit

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Welecome To 55 Blocks!:',
+              'Welecome To 55 Blocks Coding School !:',
             ),
             Text(
               '$_counter',
